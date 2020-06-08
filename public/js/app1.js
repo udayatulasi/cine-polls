b9d6360ee5e3a1d748ab4c1621cbbd48c769d933
saikumar403 <50109337+saikumar403@users.noreply.github.com>
2020-06-08 14:11:14 -0300

    modified css and js file

--- public/js/app1.js
+++ public/js/app1.js
@@ -130,8 +130,8 @@
           document.querySelector('.table').insertAdjacentHTML('beforeend', newhtml);
           
         }
-        user  = `<br><form class="form-inline primary-form" id="userName"><div class="form-group mb-2"><label for="staticEmail2" class="sr-only">Email</label><input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Username :"></div><div class="form-group mx-sm-3 mb-2"><label for="inputPassword2" class="sr-only">Password</label><input type="password" class="form-control second-submit" id="inputPassword2" placeholder="Username"></div></form>`;
-         document.querySelector('.table').insertAdjacentHTML('beforeend', user);
+        user  = `<br><form class="form-inline  form-group mx-sm-3 mb-2" id="userName"><div class="form-group mb-2"><label for="staticEmail2" class="sr-only">Email</label><input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Username :"></div><div class="form-group mx-sm-3 mb-2"><label for="inputPassword2" class="sr-only">Password</label><input type="text" class="primary-form second-submit" id="inputPassword2" placeholder="Username"></div></form>`;
+         document.querySelector('.table').insertAdjacentHTML('beforebegin', user);
       };
 
       function addDisplay () {
